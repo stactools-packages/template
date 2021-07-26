f898f9639e2af3a9f593c994cc2a5198481eb586
bluegeo <devin.cairns@bluegeo.ca>
2021-07-26 11:46:05 -0700

    Fix .package for rename

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -2,19 +2,19 @@ import os.path
 from tempfile import TemporaryDirectory
 
 import pystac
-from stactools.package.commands import create_package_command
+from stactools.package.commands import create_stactoolspackage_command
 from stactools.testing import CliTestCase
 
 
 class CreateCollectionTest(CliTestCase):
     def create_subcommand_functions(self):
-        return [create_package_command]
+        return [create_stactoolspackage_command]
 
     def test_create_collection(self):
         with TemporaryDirectory() as tmp_dir:
             # Run your custom create-collection command and validate
             # Example:
-            result = self.run_command(["package", "create-collection"])
+            result = self.run_command(["stactoolspackage", "create-collection"])
 
             self.assertEqual(result.exit_code,
                              0,
@@ -31,7 +31,7 @@ class CreateCollectionTest(CliTestCase):
         with TemporaryDirectory() as tmp_dir:
             # Run your custom create-item command and validate
             # Example:
-            result = self.run_command(["package", "create-item"])
+            result = self.run_command(["stactoolspackage", "create-item"])
             self.assertEqual(result.exit_code,
                              0,
                              msg="\n{}".format(result.output))
